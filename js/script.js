2fda470f97a1dd29dc9f1f2653c8d9f213e4827c
yeasinbinali <yeasinbinali@gmail.com>
2024-02-21 16:20:21 +0600

    selected seat added

--- js/script.js
+++ js/script.js
@@ -1,44 +1,85 @@
-let clickedSeat = [];
+const selectedSeats = [];
+let selectedCoupon = "";
 
-function handleSeatSelection(event) {
-    const selectedSeat = event.target;
+const totalSeatCount = document.getElementById("seat-count");
+const totalSeatNumber = document.getElementById("total-seat");
+const applyButton = document.getElementById("apply-btn");
+const nextButton = document.getElementById("next-btn");
+const couponInput = document.getElementById("apply-btn");
+const phoneNumberInput = document.getElementById("phone-number-input");
 
-    selectedSeat.classList.add('bg-green-600');
-    selectedSeat.classList.add('text-white');
+//after apply the id the apply coupon seCtion will be hIde
+const applySection = document.getElementById("place-coupon");
 
-    const selectedSeatNumber = selectedSeat.innerText;
+applyButton.setAttribute("disabled", true);
+nextButton.setAttribute("disabled", true);
+applyButton.style.cursor = "not-allowed";
+nextButton.style.cursor = "not-allowed";
 
+document.querySelectorAll(".seat").forEach(function (seat) {
+    seat.addEventListener("click", function () {
+        if (nextButton.getAttribute("disabled") === "true") {
+            nextButton.removeAttribute("disabled");
+            nextButton.style.cursor = "pointer";
+        }
 
-    clickedSeat.push(selectedSeat);
+        if (applyButton.getAttribute("disabled") === "true") {
+            applyButton.removeAttribute("disabled");
+        }
 
-    const totalSeat = document.getElementById('total-seat');
-    totalSeat.innerText = clickedSeat.length;
+        let isAlreadyButtonSelected = false;
+        for (let i = 0; i < selectedSeats.length; i++) {
+            if (selectedSeats[i] === seat.textContent.trim()) {
+                isAlreadyButtonSelected = true;
+                break;
+            }
+        }
 
-    const remainingSeat = document.getElementById('remaining-seat');
-    remainingSeat.innerText = remainingSeat.innerText - 1;
+        if (isAlreadyButtonSelected) {
+            const index = selectedSeats.indexOf(seat.textContent.trim());
+            selectedSeats.splice(index, 1);
+            seat.style.backgroundColor = "";
+        } else {
+            if (selectedSeats.length >= 4) {
+                alert("You can only select up to 4 seats.");
+                return;
+            }
+            seat.style.backgroundColor = "#1DD100";
+            selectedSeats.push(seat.textContent.trim());
+        }
+        totalSeatCount.textContent = selectedSeats.length;
+        totalSeatNumber.textContent = 40 - selectedSeats.length;
+        updateSeatInDisplay();
 
-    const priceContainer = document.getElementById('price-container');
+        const changingPriceTicket = document.getElementById("changing-price");
+        const ticketPrice = selectedSeats.length * 550;
+        changingPriceTicket.textContent = ticketPrice;
 
-    const seat = document.getElementById('seat');
-    const seatClass = document.getElementById('class');
-    const seatPrice = document.getElementById('price');
+        if (selectedSeats.length === 4) {
+            applyButton.removeAttribute("disabled");
+            applyButton.style.cursor = "pointer";
+        } else {
+            applyButton.setAttribute("disabled", true);
+            applyButton.style.cursor = "not-allowed";
+        }
+        document.getElementById("grand-total-price").textContent = ticketPrice;
+    });
+});
 
-    seat.innerText = selectedSeatNumber;
-    priceContainer.appendChild(seat);
+applyButton.addEventListener("click", function () {
+    const couponCode = couponInput.value.trim();
+    if (couponCode === "") {
+        return;
+    }
+    calculateTheTotalPrice(couponCode);
+});
 
-    seatClass.innerText = 'Economy';
-    priceContainer.appendChild(seatClass);
-
-    seatPrice.innerText = 500;
-    priceContainer.appendChild(seatPrice);
-
-}
-
-
-const seatElements = document.querySelectorAll('.seat');
-// seatElements.forEach(seat => {
-//     seat.addEventListener('click', handleSeatSelection);
-// });
-for (const seatElement of seatElements) {
-    seatElement.addEventListener('click', handleSeatSelection);
-}
+phoneNumberInput.addEventListener("input", function () {
+    if (selectedSeats.length > 0 && phoneNumberInput.value.trim() !== "") {
+        nextButton.removeAttribute("disabled");
+        nextButton.style.cursor = "pointer";
+    } else {
+        nextButton.setAttribute("disabled", true);
+        nextButton.style.cursor = "not-allowed";
+    }
+});
